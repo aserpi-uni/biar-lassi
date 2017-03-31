7f96ddbbe4557fcd6f0d89eaf7135fe28592a137
aserpi <alessandro.serpi@me.com>
2017-03-31 23:36:49 +0200

    reset password with username or email

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -2,8 +2,8 @@ class Employees::RegistrationsController < Devise::RegistrationsController
   include Accessible
 
   before_action :redirect_user
-# before_action :configure_sign_up_params, only: [:create]
-# before_action :configure_account_update_params, only: [:update]
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
   # def new
