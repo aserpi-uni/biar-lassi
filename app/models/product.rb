150c6f4e769ee66e93b9a7dc7c5c87ab397898a0
ccaracciolo <caracciolo.1550367@studenti.uniroma1.it>
2017-12-18 15:21:23 +0100

    Initial bootstrap graphics for product index and show. Problem with add product for consumer

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :enterprise
   has_many :problem_threads, dependent: :destroy
-  has_attached_file :image, styles: { medium: "400x400#"}
+  has_attached_file :image, styles: { medium: "200x300#"}, thumb: "100x100"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
