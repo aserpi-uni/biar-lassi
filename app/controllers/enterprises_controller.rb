579afa771ce390c371dc464000aeb1e8c5a1ecaf
Alessandro Serpi <alessandro.serpi@me.com>
2018-01-15 22:37:56 +0100

    Finalize enterprise UI and fix product UI

--- app/controllers/enterprises_controller.rb
+++ app/controllers/enterprises_controller.rb
@@ -4,7 +4,7 @@ class EnterprisesController < ApplicationController
   def show
     authorize @enterprise
 
-    @products = @enterprise.products.page(params[:page])
+    @products = @enterprise.products.order(:model).paginate(page: params[:page], per_page: 5)
   end
 
   def new
